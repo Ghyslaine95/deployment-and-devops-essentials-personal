16f6f8b73e60bc7fe194be376a5380ef8e022034
Ghyslaine95 <ghyslaineira@gmail.com>
2025-11-16 23:04:32 +0200

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    minify: 'terser', // or 'esbuild' if terser still causes issues
+    sourcemap: false
+  },
   server: {
     port: 5173,
     host: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: mode !== 'production',
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          socket: ['socket.io-client']
-        }
-      }
-    }
-  },
-  define: {
-    'process.env': {}
   }
-}))
+})
